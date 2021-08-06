b55d8f5deac22ce7b430637b4a6719c10db4caa2
Matheus Alves <matheusalves.bmd@gmail.com>
2021-08-05 23:51:40 -0300

    Calculate the winner

--- app/assets/components/Board.js
+++ app/assets/components/Board.js
@@ -9,6 +9,10 @@ class Board extends Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
+    // if the square is not "empty" or if there's a winner
+    if (squares[i] != null || this.calculateWinner(squares)) {
+      return;
+    }
     squares[i] = this.state.xIsNext ? "X" : "O";
     // "squares:squares" can be replaced by "squares"
     this.setState({ squares, xIsNext: !this.state.xIsNext });
@@ -25,9 +29,39 @@ class Board extends Component {
     );
   }
 
-  render() {
-    const status = `Next player: ${this.state.xIsNext ? "X" : "O"}`;
+  calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
 
+  render() {
+    const winner = this.calculateWinner(this.state.squares);
+    let status;
+
+    if (winner) {
+      status = "Winner: " + winner;
+    } else {
+      status = `Next player: ${this.state.xIsNext ? "X" : "O"}`;
+    }
     return (
       <div>
         <div className="status">{status}</div>
